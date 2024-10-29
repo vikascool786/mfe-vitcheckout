0fdf8d80c2cb5bc4e73015295bb7e42df3a7299f
Saahiil Shaikh <sahil@Saahiils-MacBook-Pro.local>
2024-10-29 12:51:39 +0530

    added refinements to card components

--- mfe-checkout/src/payment-method/card-information/CardInformation.tsx
+++ mfe-checkout/src/payment-method/card-information/CardInformation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../checkout/Checkout.scss";
+import "./CardInformation.scss";
 import { FormField } from "../../component/Form/Field/FormField";
 import { Checkbox } from "../../component/Form/Checkbox/Checkbox";
 
@@ -14,11 +14,19 @@ export const CardInformation = () => {
       </div>
       <div className="form-field-container">
         <FormField label="CVV" required extraLabel="3 or 4 digits" />
-        <Checkbox title="" />
+        <div className="save-for-later">
+          <input className="checkbox" type="checkbox" />
+          <span className="shipping-text">Save card for later</span>
+        </div>
       </div>
-      <div>
-        Billing Address <input type="checkbox"/> <span>Same as shipping</span> 
-       <div className="checkbox-text">Ruby Boyle, 1 Lower Ragsdale Dr. Monterey, CA 93430</div>
+      <div className="billing">
+        <div className="billing-address">
+          Billing Address <input className="checkbox" type="checkbox" />
+        </div>
+        <span className="shipping-text">Same as shipping</span>
+      </div>
+      <div className="checkbox-text">
+        Ruby Boyle, 1 Lower Ragsdale Dr. Monterey, CA 93430
       </div>
     </div>
   );
