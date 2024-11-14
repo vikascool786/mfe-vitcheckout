734dcdebb023fb106c6ddad5aaa6556258335945
vikas wankhede <vikas.cool786@gmail.com>
2024-11-14 10:09:42 +0530

    code updated

--- mfe-checkout/src/interfaces/EWallet.ts
+++ mfe-checkout/src/interfaces/EWallet.ts
@@ -1,16 +1,17 @@
 export interface EWallet {
-  lifetimeCashbackEarned: string;
-  lifetimeCoaEarned: string;
-  lifetimeTotalCashEarned: string;
-  pendingCashbackAvail: string;
-  cashbackAvail: string;
-  coaAvail: string;
-  redemptionThreshold: string;
-  totalCoaCBAvail: string;
-  pendingCoaAvail: string;
-  ytdCoaEarned: string;
-  isWalletEligible: boolean;
-  newIbv: number;
-  newCash: number;
-  showNewCashOverlay: boolean;
-}
+    lifetimeCashbackEarned: string;
+    lifetimeCoaEarned: string;
+    lifetimeTotalCashEarned: string;
+    pendingCashbackAvail: string;
+    cashbackAvail: string;
+    coaAvail: string;
+    redemptionThreshold: string;
+    totalCoaCBAvail: string;
+    pendingCoaAvail: string;
+    ytdCoaEarned: string;
+    isWalletEligible: boolean;
+    newIbv: number;
+    newCash: number;
+    showNewCashOverlay: boolean;
+  }
+  
