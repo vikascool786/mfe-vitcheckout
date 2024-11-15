79e049c57a3d9f84e10a4ddcb6bce6ea81a59c29
Saahiil Shaikh <sahil@Saahiils-MacBook-Pro.local>
2024-11-15 07:05:50 +0530

    added address code

--- mfe-checkout/src/api/service/ShopperAddressBook.ts
+++ mfe-checkout/src/api/service/ShopperAddressBook.ts
@@ -2,28 +2,74 @@ import { API_KEY, GET_API_ENDPOINT_BASE_URL } from "../../utils/ApiConstants";
 import axiosInstance from "../axios";
 
 export const fetchShopperAddressBook = async (
-    shopperId: string
+  shopperId: string
 ): Promise<any> => {
-    try {
-        const shopperAddressBookApiEndpoint = `${GET_API_ENDPOINT_BASE_URL}/shopper-addressbooks/v1/${shopperId}/AddressBook?api_key=${API_KEY}`;
-        const addressBookResponse = await axiosInstance(shopperAddressBookApiEndpoint).get("");
-        return addressBookResponse.data;
-    } catch (error) {
-        console.error(`Error getting address book for shopper: ${shopperId}`, error);
-        throw error;
-    }
+  try {
+    const shopperAddressBookApiEndpoint = `${GET_API_ENDPOINT_BASE_URL}/shopper-addressbooks/v1/${shopperId}/AddressBook?api_key=${API_KEY}`;
+    const addressBookResponse = await axiosInstance(
+      shopperAddressBookApiEndpoint
+    ).get("");
+    return addressBookResponse.data;
+  } catch (error) {
+    console.error(
+      `Error getting address book for shopper: ${shopperId}`,
+      error
+    );
+    throw error;
+  }
 };
 
 export const createShopperAddressBookEntry = async (
-    shopperId: string,
-    addressData: string
+  shopperId: string,
+  addressData: string
 ): Promise<any> => {
-    try {
-        const shopperAddressBookApiEndpoint = `${GET_API_ENDPOINT_BASE_URL}/shopper-addressbooks/v1/${shopperId}/AddressBook?api_key=${API_KEY}`;
-        const response = await axiosInstance(shopperAddressBookApiEndpoint).post("", addressData, { headers: { "Content-Type": "application/x-www-form-urlencoded", }, })
-        return response.data;
-    } catch (error) {
-        console.error(`Error creating address book entry for shopper: ${shopperId}`, error);
-        throw error;
-    }
+  try {
+    const shopperAddressBookApiEndpoint = `${GET_API_ENDPOINT_BASE_URL}/shopper-addressbooks/v1/${shopperId}/AddressBook?api_key=${API_KEY}`;
+    const response = await axiosInstance(shopperAddressBookApiEndpoint).post(
+      "",
+      addressData,
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      `Error creating address book entry for shopper: ${shopperId}`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const updateShopperAddressBookEntry = async (
+  shopperId: string,
+  addressId: number,
+  addressData: string
+): Promise<any> => {
+  try {
+    const shopperAddressBookApiEndpoint = `${GET_API_ENDPOINT_BASE_URL}/shopper-addressbooks/v1/${shopperId}/AddressBook/${addressId}?api_key=${API_KEY}`;
+    const response = await axiosInstance(shopperAddressBookApiEndpoint).put(
+      "",
+      addressData,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      `Error updating address book entry for shopper: ${shopperId}`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const updateTextUpdatesForPhone = async (phoneNumber: string) => {
+  const endpoint = `https://devapi2.shop.com/twilio/v1/lookups?type=carrier&to=${phoneNumber}&country=USA?api_key=${API_KEY}`;
+  try {
+    const response = await axiosInstance(endpoint).get("");
+    return response.data;
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
--- mfe-checkout/src/interfaces/Address.ts
+++ mfe-checkout/src/interfaces/Address.ts
@@ -1,13 +1,39 @@
+
 export interface Address {
-    id: number;
-    isPrimary: boolean;
-    first: string;
-    last: string;
-    address1: string;
-    address2: string;
-    zip: string;
-    city: string;
-    state: string;
-    phone: string;
-    country?: string;
+  isShip: number
+  isBill: number
+  isPrimary: number
+  hasAddress: number
+  id: number
+  description: string
+  prefix: string
+  first: string
+  mi: string
+  last: string
+  company: string
+  address1: string
+  address2: string
+  city: string
+  state: string
+  region: string
+  zip: string
+  country: string
+  phone: string
+  address3: string
+  address4: string
+  address5: string
+  address6: string
+  address7: string
+  zone: number
+  isPoBox: boolean
+  shopperAccountDisabled: number
+  links: Links
+  isoalpha3Code?: string
+  hashCode?: string
+}
+
+export interface Links {
+  rel: string
+  href: string
+  type: string
 }
