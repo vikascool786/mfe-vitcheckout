24cf930c5776c17f1431f289e73901b32672a36e
vikas wankhede <vikasw@marketamerica.com>
2025-04-24 00:49:31 +0530

    develop merged

--- mfe-checkout/src/checkout/TotalAmount.tsx
+++ mfe-checkout/src/checkout/TotalAmount.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './TotalAmount.scss';
-export const TotalAmount: React.FC<any> = () => {
+import { useAtom } from "jotai";
+import { orderAtom } from "../store";
+export const TotalAmount: React.FC<any> = ({ }) => {
+    const [order, setOrder] = useAtom(orderAtom);
+
     return (
         <div className="total-amount__wrapper">
             <div className="total-amount__main">
                 <div className="total-amount__label">Total</div>
-                <div className="total-amount__price">109</div>
+                <div className="total-amount__price">{`${order?.totals?.priceStr}`}</div>
             </div>
         </div>
     );
