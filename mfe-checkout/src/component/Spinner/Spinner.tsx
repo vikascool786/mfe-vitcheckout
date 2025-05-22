12b82f225b70095fe4ea239dfe7407a506e6b78c
Saahiil Shaikh <sahil@Saahiils-MacBook-Pro.local>
2025-05-22 18:58:33 +0530

    AI-111284

--- mfe-checkout/src/component/Spinner/Spinner.tsx
+++ mfe-checkout/src/component/Spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Spinner.scss";
 
 export const Spinner = () => (
-  <div className="checkout-spinner-ring">
-    <div className="checkout-spinner"></div>
-  </div>
+<div className="checkout-spinner-wrapper">
+  <div className="checkout-spinner"></div>
+</div>
 );
