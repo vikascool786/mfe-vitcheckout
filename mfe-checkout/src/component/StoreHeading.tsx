00d057399a126f68d90054ea49ce6244a45e174b
Saahiil Shaikh <sahil@Saahiils-MacBook-Pro.local>
2025-06-19 23:24:11 +0530

    AI-111386

--- mfe-checkout/src/component/StoreHeading.tsx
+++ mfe-checkout/src/component/StoreHeading.tsx
@@ -45,10 +45,26 @@ const StoreHeading: React.FC<IStoreHeadingProps> = ({ storeName, storeKey, isMAS
         }
     };
 
+        const multipleMAOOSItemInStore = (order: Order, storeKey: string): boolean => {
+        const store: OrderStore | null = getStoreDataFromKey(order, storeKey);
+        const multipleItemsInStore = store?.items.length  && store?.items.length > 1;
+        if(multipleItemsInStore){
+            return hasEstimatedShipDate(store?.items[0] || null); 
+        } else{
+            return false;
+        }
+    };
+
     useEffect(() => {
         let consolidateData = getOrderConsolidateData(order,getString);
         setOrderConsolidateData(consolidateData);
+        // if there is only one MA OOS item in store, show the shipment heading
+        if ((isOnlySingleMAOOSItemInStore(order, storeKey) && !isOrderSummary) || !orderIsMAOnly(order) || consolidateData.oosConsolidate === OOS_CONSOLIDATE_SPLIT_CODE || orderHasGiftCards(order)) {
         setShowStoreShipmentHeading((isOnlySingleMAOOSItemInStore(order, storeKey) && !isOrderSummary) || !orderIsMAOnly(order) || consolidateData.oosConsolidate === OOS_CONSOLIDATE_SPLIT_CODE || orderHasGiftCards(order));
+        } else {
+        // if there are multiple MA OOS items in store, show the shipment heading as per the condition
+        setShowStoreShipmentHeading((multipleMAOOSItemInStore(order, storeKey) && !isOrderSummary) || !orderIsMAOnly(order) || consolidateData.oosConsolidate === OOS_CONSOLIDATE_SPLIT_CODE || orderHasGiftCards(order));
+        }
     }, [order, storeKey]);
 
     const getShipmentNumber = (order: Order, key: string): number => {
@@ -63,6 +79,11 @@ const StoreHeading: React.FC<IStoreHeadingProps> = ({ storeName, storeKey, isMAS
         if(isMAStore && isOnlySingleMAOOSItemInStore(order, storeKey) && oosConsolidateData?.oosConsolidate !== OOS_CONSOLIDATE_SPLIT_CODE && !isOrderSummary){
             const estimatedShipDate = getItemEstimatedShipDate(getStoreDataFromKey(order, storeKey)?.items[0] || null);
             storeHeading = `${getString("shippingOn")} ${estimatedShipDate}`;
+        // condition when there are multiple MA oos items to show ship date - AI-111386
+        }
+        else if(isMAStore && multipleMAOOSItemInStore(order, storeKey) && oosConsolidateData?.oosConsolidate !== OOS_CONSOLIDATE_SPLIT_CODE && !isOrderSummary){
+            storeHeading = `${getString("shippingOn")} ${oosConsolidateData.availabilityDate}`;
+        
         } else if(oosConsolidateData?.oosConsolidate === OOS_CONSOLIDATE_SPLIT_CODE && isMAStore && !isGiftCardForStoreKey(order, storeKey)){
             let shipmentNumber = getShipmentNumber(order, storeKey);
             storeHeading = `${storeName} ${shipmentNumber}`;
